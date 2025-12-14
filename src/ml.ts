930ca6181797916dc9920112cc25e526fe283289
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2025-12-14 21:48:51 +0700

    refactor: update predict function comments to clarify image handling with Blob

--- src/ml.ts
+++ src/ml.ts
@@ -14,6 +14,11 @@ export async function loadModel(global: any) {
 
 // Predict function
 export const predict = async (filepath: string, model: any) => {
+  // Instead of using the filepath which requires the image to be saved locally first, I can use Blob.
+  // See https://github.com/huggingface/transformers.js/blob/b125e82b86f62cf9f0f77217601e04a5ff7c6e7f/src/utils/image.js#L117
+  // const base64 = ...; // get base64 string from somewhere
+  // const imageBuffer = Buffer.from(base64, 'base64');
+  // const imageBlob = new Blob([imageBuffer], { type: 'image/png' });
   const results = (await model(filepath, { threshold: 0.9 })) as Prediction[];
   logger(results);
   return results;
